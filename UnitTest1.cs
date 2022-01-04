62f6592a289afde439c0d98d464628a65bcb2f4c
John.michael <John.michael@studentambassadors.com>
2022-01-04 17:58:41 -0600

    added a test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Calculator;
 
 namespace UnitTestsalary
 {
     [TestClass]
     public class UnitTest1
     {
+        //to get hourly, divide anual salary by 2080
+        //100,006.40/ 2080=48.08 hr
+        //to get annual pay 48.08hr*2080=100,006.40
         [TestMethod]
-        public void TestMethod1()
+        public void AnnualSalaryTest()
         {
+            //arrange
+            SalaryCalculator ts = new SalaryCalculator();
+            //act
+            decimal annualSalary = ts.GetAnnualSalary(50);
+            //assert
+            Assert.AreEqual(104000, annualSalary);
         }
     }
 }
